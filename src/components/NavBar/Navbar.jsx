01217d8963dcc197edfce0903bbb171163e00b43
Sofa <sofa5060@gmail.com>
2023-04-27 18:19:31 +0200

    Added language to navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,32 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.svg";
-import './Navbar.css'
+import "./Navbar.css";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from '@mui/material/FormControl';
 
-export default function Navbar({currPage}) {
+export default function Navbar({ currPage }) {
+  const [language, setLanguage] = useState("EN");
   return (
     <div className="navbar-background">
       <div className="navbar">
         <div className="logo">
-          <Link to='/'>
+          <Link to="/">
             <img src={logo} alt="logo" />
           </Link>
         </div>
         <ul className="navbar-list">
-          <li><Link to='/' className={currPage==="home" ? "main" : ""}>Home</Link></li>
-          <li><Link to='/trips' className={currPage==="trips" ? "main" : ""}>Trips</Link></li>
-          <li><Link to='/about' className={currPage==="about" ? "main" : ""}>About Us</Link></li>
-          <li><Link to='/contact' className={currPage==="contact" ? "main" : ""}>Contact Us</Link></li>
-        </ul>
-        <ul className="navbar-user">
           <li>
-            <Link to='/'>LOGIN</Link>
+            <Link to="/" className={currPage === "home" ? "main" : ""}>
+              Home
+            </Link>
           </li>
-          <li>/</li>
           <li>
-            <Link to='/'>SIGNUP</Link>
+            <Link to="/trips" className={currPage === "trips" ? "main" : ""}>
+              Trips
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className={currPage === "about" ? "main" : ""}>
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              className={currPage === "contact" ? "main" : ""}
+            >
+              Contact Us
+            </Link>
           </li>
         </ul>
+        <div className="navbar-language">
+          <FormControl variant="standard" sx={{ m: 1 }}>
+            <Select
+              labelId="demo-simple-select-standard-label"
+              id="demo-simple-select-standard"
+              value={language}
+              onChange={(e) => setLanguage(e.target.value)}
+              label="Language"
+              disableUnderline={true}
+            >
+              <MenuItem value="EN">EN</MenuItem>
+              <MenuItem value="ES">ES</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
       </div>
     </div>
   );
