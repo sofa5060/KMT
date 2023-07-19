5de3bb5bd6445cccd1e672123815e3b23538e820
Sofa <sofa5060@gmail.com>
2023-07-19 14:49:03 +0300

    Translated Contact Page

--- src/components/ContactForm/Contactform.jsx
+++ src/components/ContactForm/Contactform.jsx
@@ -1,8 +1,9 @@
-import React, {useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import "./Contactform.css";
 import TextField from "@mui/material/TextField";
 import { AlertContext } from "../../context/AlertContextProvider";
 import axios from "axios";
+import { LanguageContext } from "../../context/LanguageContextProvider";
 
 export default function Contactform() {
   const [firstName, setFirstName] = useState("");
@@ -11,23 +12,23 @@ export default function Contactform() {
   const [message, setMessage] = useState("");
 
   const { showAlert } = useContext(AlertContext);
+  const { renderContent } = useContext(LanguageContext);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if(!firstName || !lastName || !emailAddress || !message){
+    if (!firstName || !lastName || !emailAddress || !message) {
       return showAlert("error", "Please fill all the fields");
     }
 
-
     let res;
-    try{
+    try {
       await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/contact`, {
         name: firstName + " " + lastName,
         email: emailAddress,
-        message
+        message,
       });
-    }catch(e){
+    } catch (e) {
       return showAlert("error", "Error in submitting the form");
     }
 
@@ -40,22 +41,69 @@ export default function Contactform() {
 
   return (
     <div className="contact-form">
-      <h2>Connect with KMT Tours Get in Touch Today!</h2>
+      <h2>
+        {renderContent(
+          "Connect with KMT Tours Get in Touch Today!",
+          "Conéctate con KMT Tours ¡Ponte en contacto hoy!",
+          "Conecte-se com KMT Tours Entre em contato hoje!"
+        )}
+      </h2>
       <p>
-        Whether you have questions about our tours, want to request a quote, or
-        simply need more information about traveling to Egypt, we're here to
-        help. Our team of friendly and knowledgeable travel experts is available
-        to assist you with all your travel needs. Contact us today and let us
-        help you plan an unforgettable trip to Egypt!
+        {renderContent(
+          "Whether you have questions about our tours, want to request a quote, or simply need more information about traveling to Egypt, we're here to help. Our team of friendly and knowledgeable travel experts is available to assist you with all your travel needs. Contact us today and let us help you plan an unforgettable trip to Egypt!",
+          "Ya sea que tengas preguntas sobre nuestros tours, quieras solicitar una cotización, o simplemente necesites más información sobre cómo viajar a Egipto, estamos aquí para ayudarte. Nuestro equipo de expertos en viajes amigables y conocedores está disponible para ayudarte con todas tus necesidades de viaje. Contáctanos hoy y permítenos ayudarte a planificar un viaje inolvidable a Egipto!",
+          "Se você tem perguntas sobre nossos passeios, quer solicitar uma cotação, ou simplesmente precisa de mais informações sobre como viajar para o Egito, estamos aqui para ajudar. Nossa equipe de especialistas em viagens amigáveis e conhecedores está disponível para ajudá-lo com todas as suas necessidades de viagem. Contate-nos hoje e deixe-nos ajudá-lo a planejar uma viagem inesquecível para o Egito!"
+        )}
       </p>
       <form onSubmit={handleSubmit} className="form">
         <div className="row">
-          <TextField label="First Name" required fullWidth onChange={e => setFirstName(e.target.value)} value={firstName}/>
-          <TextField label="Last Name" required fullWidth onChange={e => setLastName(e.target.value)} value={lastName}/>
+          <TextField
+            label={renderContent(
+              "First Name",
+              "Primer nombre",
+              "Primeiro nome"
+            )}
+            required
+            fullWidth
+            onChange={(e) => setFirstName(e.target.value)}
+            value={firstName}
+          />
+          <TextField
+            label={renderContent("Last Name", "Apellido", "Último nome")}
+            required
+            fullWidth
+            onChange={(e) => setLastName(e.target.value)}
+            value={lastName}
+          />
         </div>
-        <TextField label="Email Address" type="email" required fullWidth onChange={e => setEmailAddress(e.target.value)} value={emailAddress}/>
-        <TextField label="Write Your Message" multiline required fullWidth rows={5} onChange={e => setMessage(e.target.value)} value={message}/>
-        <button className="btn">Submit</button>
+        <TextField
+          label={renderContent(
+            "Email Address",
+            "Dirección de correo electrónico",
+            "Endereço de e-mail"
+          )}
+          type="email"
+          required
+          fullWidth
+          onChange={(e) => setEmailAddress(e.target.value)}
+          value={emailAddress}
+        />
+        <TextField
+          label={renderContent(
+            "Write Your Message",
+            "Escribe tu mensaje",
+            "Escreva sua mensagem"
+          )}
+          multiline
+          required
+          fullWidth
+          rows={5}
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+        <button className="btn">
+          {renderContent("Submit", "Enviar", "Enviar")}
+        </button>
       </form>
     </div>
   );
--- src/pages/ContactPage/Contactpage.jsx
+++ src/pages/ContactPage/Contactpage.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import "./Contactpage.css";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import PhoneIcon from "@mui/icons-material/Phone";
 import EmailIcon from "@mui/icons-material/Email";
 import Contactform from "../../components/ContactForm/Contactform";
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import { LanguageContext } from "../../context/LanguageContextProvider";
 
 export default function Contactpage({ setCurrPage }) {
+  const { renderContent } = useContext(LanguageContext);
   useEffect(() => {
     setCurrPage("contact");
   }, []);
@@ -15,8 +17,20 @@ export default function Contactpage({ setCurrPage }) {
     <div className="contact-page">
       <div className="left-col">
         <div className="section">
-          <h3>Chat with us</h3>
-          <p>Instant Support for Your Travel Needs</p>
+          <h3>
+            {renderContent(
+              "Chat with us",
+              "Chatea con nosotros",
+              "Converse conosco"
+            )}
+          </h3>
+          <p>
+            {renderContent(
+              "Instant Support for Your Travel Needs",
+              "Soporte instantáneo para tus necesidades de viaje",
+              "Suporte instantâneo para suas necessidades de viagem"
+            )}
+          </p>
           <div className="row">
             <EmailIcon />
             <a href="mailto:info@kmttoursegypt.com" className="text">
@@ -25,18 +39,34 @@ export default function Contactpage({ setCurrPage }) {
           </div>
         </div>
         <div className="section">
-          <h3>Visit us</h3>
-          <p>Meet Our Travel Experts in Person</p>
+          <h3>{renderContent("Visit us", "Visítanos", "Visite-nos")}</h3>
+          <p>
+            {renderContent(
+              "Meet Our Travel Experts in Person",
+              "Conoce a nuestros expertos en viajes en persona",
+              "Conheça nossos especialistas em viagens pessoalmente"
+            )}
+          </p>
           <div className="row">
             <LocationOnIcon />
             <p className="text">
-              8 Gawad Housny, st، Abdeen, Cairo Governorate 4280143
+              {renderContent(
+                "8 Gawad Housny st, Abdeen, Cairo Governorate 4280143",
+                "8 Gawad Housny st, Abdeen, Gobernación de El Cairo 4280143",
+                "8 Gawad Housny st, Abdeen, Governadoria do Cairo 4280143"
+              )}
             </p>
           </div>
         </div>
         <div className="section">
-          <h3>Call us</h3>
-          <p>Speak To Our Travel Experts Today</p>
+          <h3>{renderContent("Call us", "Llámanos", "Ligue para nós")}</h3>
+          <p>
+            {renderContent(
+              "Speak To Our Travel Experts Today",
+              "Habla con nuestros expertos en viajes hoy",
+              "Fale com nossos especialistas em viagens hoje"
+            )}
+          </p>
           <div className="row">
             <PhoneIcon />
             <a href="tel:+20223918470" className="text">
@@ -45,11 +75,16 @@ export default function Contactpage({ setCurrPage }) {
           </div>
         </div>
         <div className="section">
-          <h3>Reach us</h3>
-          <p>We Even Available On Whatsapp</p>
+          <h3>{renderContent("Reach us", "Contáctanos", "Alcance-nos")}</h3>
+          <p>{renderContent("We Even Available On Whatsapp", "Incluso estamos disponibles en Whatsapp", "Estamos até disponíveis no Whatsapp")}</p>
           <div className="row">
             <WhatsAppIcon />
-            <a href="https://api.whatsapp.com/send?phone=201142636629" className="text" target="_blank" rel="noreferrer">
+            <a
+              href="https://api.whatsapp.com/send?phone=201142636629"
+              className="text"
+              target="_blank"
+              rel="noreferrer"
+            >
               (+20) 1142636629
             </a>
           </div>
